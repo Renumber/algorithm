85117c3a17f2fc71495292cd91298fe71198d1a6
renumber <jwook9331@naver.com>
2022-06-29 09:58:19 +0900

    [10998] 22/06/29

---
+++ 10998/10998.cpp
@@ -0,0 +1,16 @@
+﻿/*
+두 정수 A와 B를 입력받은 다음, A×B를 출력하는 프로그램을 작성하시오.
+https://www.acmicpc.net/problem/10998
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+    cin >> num1;
+    cin >> num2;
+    cout << num1 * num2;
+}
