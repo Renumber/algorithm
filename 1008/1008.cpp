fb5c099c0229e7441f07496fb46c77042caf60eb
renumber <jwook9331@naver.com>
2022-06-30 19:28:24 +0900

    [1008] 22/06/30

---
+++ 1008/1008.cpp
@@ -0,0 +1,17 @@
+﻿/*
+두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
+https://www.acmicpc.net/problem/1008
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double num1, num2;
+    cin >> num1;
+    cin >> num2;
+    cout.precision(30);
+    cout << (num1 / num2);
+}
