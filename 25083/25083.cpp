8f48a601770baadb6a80a34dcab63f49fb6dd0fd
renumber <jwook9331@naver.com>
2022-07-01 10:24:52 +0900

    [25083] 22/07/01

---
+++ 25083/25083.cpp
@@ -0,0 +1,25 @@
+﻿/*
+아래 예제와 같이 새싹을 출력하시오.
+         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |
+https://www.acmicpc.net/problem/25083
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "         ,r'\"7" << endl;
+    cout << "r`-_   ,'  ,/" << endl;
+    cout << " \\. \". L_r'" << endl;
+    cout << "   `~\\/" << endl;
+    cout << "      |" << endl;
+    cout << "      |" << endl;
+}
+
