9b715ca11e8f0c5baccb9e8d3cc3ff9fa2b5c400
renumber <jwook9331@naver.com>
2022-07-08 10:07:54 +0900

    [10950] 22/07/08

---
+++ ch3.iteration/10950/10950.cpp
@@ -0,0 +1,21 @@
+﻿/*
+두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+https://www.acmicpc.net/problem/10950
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int cnt, num1, num2;
+	cin >> cnt;
+
+	for (int i = 0; i < cnt; i++) {
+		cin >> num1;
+		cin >> num2;
+		cout << num1 + num2 << endl;
+	}
+}
+
