dd19317af01d97a1155faa16dad564f682320366
renumber <jwook9331@naver.com>
2022-07-11 09:58:02 +0900

    [8393] 22/07/11

---
+++ ch3.iteration/8393/8393.cpp
@@ -0,0 +1,18 @@
+﻿/*
+n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램을 작성하시오.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int num, cnt = 0;
+	cin >> num;
+
+	for (int i = 1; i <= num; i++) {
+		cnt += i;
+	}
+	cout << cnt;
+}
