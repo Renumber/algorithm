a45c2ab91008163d23430f842b24dbcfad03b1d5
renumber <jwook9331@naver.com>
2022-07-11 10:09:16 +0900

    [2741] 22/07/11

---
+++ ch3.iteration/2741/2741.cpp
@@ -0,0 +1,21 @@
+﻿/*
+자연수 N이 주어졌을 때, 1부터 N까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
+
+https://www.acmicpc.net/problem/2741
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int cnt;
+
+	cin >> cnt;
+
+	for (int i = 1; i <= cnt; i++) {
+		cout << i << "\n";
+	}
+}
