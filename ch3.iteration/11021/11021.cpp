a0eff8f5fc4d5c5e2034e53042179a10754dd35e
renumber <jwook9331@naver.com>
2022-07-12 10:05:02 +0900

    [11021] 22/07/12

---
+++ ch3.iteration/11021/11021.cpp
@@ -0,0 +1,21 @@
+﻿/*
+두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int cnt, num1, num2;
+
+	cin >> cnt;
+
+	for (int i = 0; i < cnt; i++) {
+		cin >> num1;
+		cin >> num2;
+		cout << "Case #" << i + 1 << ": " << num1 + num2 << "\n";
+	}
+}
