8d307097dd41a81fd0f5559f084b3f9fdb8a58f2
renumber <jwook9331@naver.com>
2022-07-13 09:58:17 +0900

    [10871] 22/07/13

---
+++ ch3.iteration/10871/10871.cpp
@@ -0,0 +1,25 @@
+﻿/*
+정수 N개로 이루어진 수열 A와 정수 X가 주어진다. 이때, A에서 X보다 작은 수를 모두 출력하는 프로그램을 작성하시오.
+
+https://www.acmicpc.net/problem/10871
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int cnt, max, num;
+
+	cin >> cnt;
+	cin >> max;
+
+	for (int i = 0; i < cnt; i++) {
+		cin >> num;
+		if (num < max) {
+			cout << num << " ";
+		}
+	}
+}
