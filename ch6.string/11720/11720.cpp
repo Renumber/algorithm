cbdcb0f8904d7662055f3fa1287eae7923c817bf
renumber <jwook9331@naver.com>
2022-07-22 09:08:08 +0900

    [11720] 22/07/22

---
+++ ch6.string/11720/11720.cpp
@@ -0,0 +1,26 @@
+﻿/*
+N개의 숫자가 공백 없이 쓰여있다. 이 숫자를 모두 합해서 출력하는 프로그램을 작성하시오.
+
+https://www.acmicpc.net/problem/11720
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+
+int main()
+{
+	int size, sum = 0;
+	string str;
+	cin >> size;
+	cin >> str;
+
+	for (int i = 0; i < size; i++) {
+		sum += str.back() - '0';
+		str.pop_back();
+	}
+	cout << sum;
+}
