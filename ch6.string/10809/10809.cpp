331d2856c23694cd4cae46d4467bd53d6d098c66
renumber <jwook9331@naver.com>
2022-07-25 11:33:47 +0900

    [10809] 22/07/25

---
+++ ch6.string/10809/10809.cpp
@@ -0,0 +1,28 @@
+﻿/*
+알파벳 소문자로만 이루어진 단어 S가 주어진다. 각각의 알파벳에 대해서, 단어에 포함되어 있는 경우에는 처음 등장하는 위치를, 포함되어 있지 않은 경우에는 -1을 출력하는 프로그램을 작성하시오.
+
+https://www.acmicpc.net/problem/10809
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	vector<int>res(26, -1);
+	string str;
+
+	cin >> str;
+
+	for (int i = 0; i < str.size(); i++) {
+		if (res[str[i] - 'a'] == -1) {
+			res[str[i] - 'a'] = i;
+		}
+	}
+	for (int num : res) {
+		cout << num << " ";
+	}
+}
