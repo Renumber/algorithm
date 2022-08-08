3ec15c842ea0263f1b157d1ce255566a8990dfd3
renumber <jwook9331@naver.com>
2022-08-08 09:49:00 +0900

    [1978] 22/08/08

---
+++ ch8.basic math2/1978/1978.cpp
@@ -0,0 +1,33 @@
+﻿/*
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+bool check(int num) {
+	if (num == 1)
+		return false;
+	for (int i = 2; i < num; i++) {
+		if (num % i == 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+int main()
+{
+	int n, num, cnt = 0;
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> num;
+		if (check(num)) {
+			cnt++;
+		}
+	}
+	cout << cnt;
+}
---
+++ ch8.basic math2/README.md
@@ -0,0 +1,3 @@
+# 기본 수학 2
+
+https://www.acmicpc.net/step/10
