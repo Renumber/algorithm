928e0ae77ffaa0dd915129aa2c362b071e445b9e
renumber <jwook9331@naver.com>
2022-08-10 09:49:06 +0900

    [11653] 22/08/10

---
+++ ch8.basic math2/11653/11653.cpp
@@ -0,0 +1,30 @@
+﻿/*
+정수 N이 주어졌을 때, 소인수분해하는 프로그램을 작성하시오.
+
+https://www.acmicpc.net/problem/11653
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n, temp = 2;
+
+	cin >> n;
+
+	if (n == 1) {
+		return 0;
+	}
+
+	while (n != 1) {
+		if (n % temp == 0) {
+			cout << temp << '\n';
+			n /= temp;
+		}
+		else {
+			temp++;
+		}
+	}
+}
