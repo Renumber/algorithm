a390a9886ac4bb895f867fafd3c1763841b9cd48
renumber <jwook9331@naver.com>
2022-08-12 09:29:54 +0900

    [1929] 22/08/12

---
+++ ch8.basic math2/1929/1929.cpp
@@ -0,0 +1,34 @@
+﻿/*
+M이상 N이하의 소수를 모두 출력하는 프로그램을 작성하시오.
+
+https://www.acmicpc.net/problem/1929
+*/
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+bool nonp[1000000] = {};
+
+int main()
+{
+	int m, n;
+
+	nonp[0] = true;
+	nonp[1] = true;
+
+	cin >> m >> n;
+
+	for (int i = 2; i <= sqrt(n); i++) {
+		if (nonp[i] == false) {
+			for (int j = i * 2; j <= n; j += i) {
+				nonp[j] = true;
+			}
+		}
+	}
+	for (int i = m; i <= n; i++) {
+		if (nonp[i] == false) {
+			cout << i << '\n';
+		}
+	}
+}
