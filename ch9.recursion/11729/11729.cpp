afddcd4b5f39127fcb0e1cf3853a0c03c166404b
renumber <jwook9331@naver.com>
2022-08-17 12:40:36 +0900

    [11729] 22/08/17

---
+++ ch9.recursion/11729/11729.cpp
@@ -0,0 +1,27 @@
+﻿/*
+
+*/
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void rec(int from, int to, int other, int num) {
+    if (num == 1) {
+        cout << from << ' ' << to << '\n';
+    }
+    else {
+        rec(from, other, to, num - 1);
+        cout << from << ' ' << to << '\n';
+        rec(other, to, from, num - 1);
+    }
+}
+
+int main() {
+    int num;
+    cin >> num;
+    cout << (int)pow(2, num) - 1 << '\n';
+
+    rec(1, 3, 2, num);
+}
