7c0ae12a8f5190157a0995f80cd472131ea8e078
renumber <jwook9331@naver.com>
2022-08-26 11:26:14 +0900

    [10989] 22/08/26

---
+++ ch11.sort/10989/10989.cpp
@@ -0,0 +1,30 @@
+﻿/*
+N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
+
+https://www.acmicpc.net/problem/10989
+*/
+
+#include <iostream>
+using namespace std;
+
+int arr[10001] = {};
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	int n, temp;
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> temp;
+		arr[temp]++;
+	}
+
+	for (int i = 1; i < 10001; i++) {
+		for (int j = 0; j < arr[i]; j++) {
+			cout << i << '\n';
+		}
+	}
+
+}
