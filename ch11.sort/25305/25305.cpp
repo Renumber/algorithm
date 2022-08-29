edce97c029632cbe5b5a873f9b3fd35612f4e180
renumber <jwook9331@naver.com>
2022-08-29 11:41:44 +0900

    [25305] 22/08/29

---
+++ ch11.sort/25305/25305.cpp
@@ -0,0 +1,32 @@
+﻿/*
+2022 연세대학교 미래캠퍼스 슬기로운 코딩생활에 N명의 학생들이 응시했다.
+
+이들 중 점수가 가장 높은 k명은 상을 받을 것이다. 이 때, 상을 받는 커트라인이 몇 점인지 구하라.
+
+커트라인이란 상을 받는 사람들 중 점수가 가장 가장 낮은 사람의 점수를 말한다.
+
+https://www.acmicpc.net/problem/25305
+*/
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int arr[1000] = {};
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	int n, k, x;
+
+	cin >> n >> k;
+
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	sort(arr, arr + n);
+
+	cout << arr[n - k];
+
+}
