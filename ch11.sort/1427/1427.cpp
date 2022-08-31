163e0e5d31a8cd23cf4693e1aa3f600c9990b5b7
renumber <jwook9331@naver.com>
2022-08-31 10:29:15 +0900

    [1427] 22/08/31

---
+++ ch11.sort/1427/1427.cpp
@@ -0,0 +1,33 @@
+﻿/*
+배열을 정렬하는 것은 쉽다. 수가 주어지면, 그 수의 각 자리수를 내림차순으로 정렬해보자.
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+bool compare(int i, int j) {
+	return j < i;
+}
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	int n;
+	vector<int> vec;
+
+	cin >> n;
+
+	while (n > 0) {
+		vec.push_back(n % 10);
+		n = n / 10;
+	}
+
+	sort(vec.begin(), vec.end(), compare);
+
+	for (int i = 0; i < vec.size(); i++) {
+		cout << vec[i];
+	}
+}
