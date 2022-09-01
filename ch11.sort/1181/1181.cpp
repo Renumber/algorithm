7737da8a079b1219b4ee63ae43cade0d73cc8857
renumber <jwook9331@naver.com>
2022-09-01 11:12:46 +0900

    [1181] 22/09/01

---
+++ ch11.sort/1181/1181.cpp
@@ -0,0 +1,47 @@
+﻿/*
+알파벳 소문자로 이루어진 N개의 단어가 들어오면 아래와 같은 조건에 따라 정렬하는 프로그램을 작성하시오.
+
+	1. 길이가 짧은 것부터
+	2. 길이가 같으면 사전 순으로
+
+https://www.acmicpc.net/problem/1181
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+using namespace std;
+
+bool compare(string a, string b) {
+	if (a.size() < b.size()) {
+		return true;
+	}
+	else if (a.size() > b.size()) {
+		return false;
+	}
+	else {
+		return a < b;
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	vector<string> vec;
+	string temp;
+	int n;
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> temp;
+		vec.push_back(temp);
+	}
+	sort(vec.begin(), vec.end(), compare);
+
+	vec.erase(unique(vec.begin(), vec.end()), vec.end());
+
+	for (int i = 0; i < vec.size(); i++) {
+		cout << vec[i] << '\n';
+	}
+}
