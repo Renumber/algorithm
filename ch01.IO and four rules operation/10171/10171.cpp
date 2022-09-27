11d4534dbc99b341460a80a25afa55b5b4570f38
renumber <jwook9331@naver.com>
2022-09-27 10:53:07 +0900

    [ch01] reformat files

---
+++ ch01.IO and four rules operation/10171/10171.cpp
@@ -0,0 +1,19 @@
+﻿/*
+아래 예제와 같이 고양이를 출력하시오.
+\    /\
+ )  ( ')
+(  /  )
+ \(__)|
+
+https://www.acmicpc.net/problem/10171
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "\\    /\\\n";
+    std::cout << " )  ( ')\n";
+    std::cout << "(  /  ) \n";
+    std::cout << " \\(__)| \n";
+}
---
+++ ch01.IO and four rules operation/2557/2557.cpp
@@ -0,0 +1,12 @@
+﻿/*
+Hello World!를 출력하시오.
+
+https://www.acmicpc.net/problem/2557
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
