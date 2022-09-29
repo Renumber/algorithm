fca2f6b2d8477284201b294d157e38c4825064f2
renumber <jwook9331@naver.com>
2022-09-29 11:57:28 +0900

    [3036] 22/09/29

---
+++ ch14. number theory and combinatorics/3036/3036.cpp
@@ -0,0 +1,43 @@
+﻿/*
+상근이는 창고에서 링 N개를 발견했다. 상근이는 각각의 링이 앞에 있는 링과 뒤에 있는 링과 접하도록 바닥에 내려놓았다.
+
+상근이는 첫 번째 링을 돌리기 시작했고, 나머지 링도 같이 돌아간다는 사실을 발견했다. 나머지 링은 첫 번째 링 보다 빠르게 돌아가기도 했고, 느리게 돌아가기도 했다. 이렇게 링을 돌리다 보니 첫 번째 링을 한 바퀴 돌리면, 나머지 링은 몇 바퀴 도는지 궁금해졌다.
+
+링의 반지름이 주어진다. 이때, 첫 번째 링을 한 바퀴 돌리면, 나머지 링은 몇 바퀴 돌아가는지 구하는 프로그램을 작성하시오.
+
+https://www.acmicpc.net/problem/3036
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int gcd(int a, int b) {
+	int c = a % b;
+	while (c != 0) {
+		a = b;
+		b = c;
+		c = a % b;
+	}
+	return b;
+}
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int n, temp;
+	vector<int> v;
+
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> temp;
+		v.push_back(temp);
+	}
+
+	for (int i = 1; i < n; i++) {
+		temp = gcd(v[0], v[i]);
+		cout << v[0] / temp << '/' << v[i] / temp << '\n';
+	}
+
+}
