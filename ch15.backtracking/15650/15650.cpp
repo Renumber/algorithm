c43363b390d2e89410a65420f450fec80b8b2bd0
renumber <jwook9331@naver.com>
2022-10-17 10:22:37 +0900

    [15650] 22/10/17

---
+++ ch15.backtracking/15650/15650.cpp
@@ -0,0 +1,43 @@
+﻿/*
+자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
+	1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
+	고른 수열은 오름차순이어야 한다.
+
+https://www.acmicpc.net/problem/15650
+*/
+
+
+#include <iostream>
+
+using namespace std;
+
+int arr[8] = { 0, };
+bool visited[8] = { false };
+
+void dfs(int n, int m, int a, int index) {
+	if (m == index) {
+		for (int i = 0; i < m; i++) {
+			cout << arr[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+	for (int i = a; i <= n; i++) {
+		if (!visited[i]) {
+			visited[i] = true;
+			arr[index] = i;
+			dfs(n, m, i + 1, index + 1);
+			visited[i] = false;
+		}
+	}
+}
+
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	int n, m;
+
+	cin >> n >> m;
+	dfs(n, m, 1, 0);
+}
