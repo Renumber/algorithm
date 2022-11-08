2c8321e27bdefec485d7a5dab16e83d29fd74620
Renumber <renumber20@gmail.com>
2022-11-08 12:56:08 +0900

    [9251] 22/11/08

---
+++ ch16.dynamic programming 1/9251/9251.cpp
@@ -0,0 +1,38 @@
+﻿/*
+LCS(Longest Common Subsequence, 최장 공통 부분 수열)문제는 두 수열이 주어졌을 때, 모두의 부분 수열이 되는 수열 중 가장 긴 것을 찾는 문제이다.
+
+예를 들어, ACAYKP와 CAPCAK의 LCS는 ACAK가 된다.
+
+https://www.acmicpc.net/problem/9251
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+int dp[1001][1001];
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    int maxRes = 1;
+
+    string str1, str2;
+
+    cin >> str1 >> str2;
+
+    for (int i = 1; i <= str1.size(); i++) {
+        for (int j = 1; j <= str2.size(); j++) {
+            if (str1[i - 1] == str2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            }
+            else {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+
+        }
+    }
+
+    cout << dp[str1.size()][str2.size()];
+}
