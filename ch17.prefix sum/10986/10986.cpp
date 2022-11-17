4367f7930b3402f0ed87b173931190f94edd1f3c
Renumber <renumber20@gmail.com>
2022-11-17 12:00:09 +0900

    [10986] 11/10/17

---
+++ ch17.prefix sum/10986/10986.cpp
@@ -0,0 +1,39 @@
+﻿/*
+수 N개 A1, A2, ..., AN이 주어진다. 이때, 연속된 부분 구간의 합이 M으로 나누어 떨어지는 구간의 개수를 구하는 프로그램을 작성하시오.
+
+즉, Ai + ... + Aj (i ≤ j) 의 합이 M으로 나누어 떨어지는 (i, j) 쌍의 개수를 구해야 한다.
+
+https://www.acmicpc.net/problem/10986
+*/
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+long long cnt[1001];
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int n, m;
+    long long sum = 0, temp, res = 0;
+
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        cin >> temp;
+        sum = (temp + sum) % m;
+        if (sum == 0) {
+            res++;
+        }
+
+        cnt[sum]++;
+    }
+
+    for (int i = 0; i < m; i++) {
+        res += cnt[i] * (cnt[i] - 1) / 2;
+    }
+
+    cout << res;
+
+
+}
