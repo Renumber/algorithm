935272510d274e1a0714c3a918d906dbf7a8e4d1
Renumber <renumber20@gmail.com>
2022-12-13 13:27:25 +0900

    [2164] 22/12/13

---
+++ ch21.queue/2164/2164.cpp
@@ -0,0 +1,31 @@
+﻿/*
+
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    int n, temp;
+    queue<int> q;
+
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        q.push(i);
+    }
+
+    for (int i = 0; i < n - 1; i++) {
+        q.pop();
+        temp = q.front();
+        q.pop();
+        q.push(temp);
+    }
+
+    cout << q.front();
+}
